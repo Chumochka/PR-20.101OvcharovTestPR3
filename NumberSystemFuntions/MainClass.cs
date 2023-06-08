159fd46affe0ca4bcd3710e422487de3a2c28e73
Chumochka <danpro1010@bk.ru>
2023-06-08 18:16:11 +0700

    1

---
+++ NumberSystemFuntions/MainClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberSystemFuntions
+{
+    public class MainClass
+    {
+
+        public Int64 DecimaltoBinary(int num)
+        {
+            string binary = "";
+
+            while (num > 0)
+            {
+                binary = (num % 2).ToString() + binary;
+                num /= 2;
+            }
+            return Convert.ToInt64(binary);
+        }
+        public int DecimaltoOctal(int num)
+        {
+            string octal = "";
+
+            while (num > 0)
+            {
+                octal = (num % 8).ToString() + octal;
+                num /= 8;
+            }
+            return Convert.ToInt32(octal);
+        }
+        public string DecimalToHexadecimal(int num)
+        {
+            string hexadicimal = "";
+            string alp = "0123456789ABCDEF";
+
+            while (num > 0)
+            {
+                hexadicimal = alp[num % 16] + hexadicimal;
+                num /= 16;
+            }
+            return hexadicimal;
+        }
+    }
+}
---
+++ NumberSystemTests/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace NumberSystemTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DecimaltoBinaryTest()
+        {
+            int num = 25;
+            Int64 expected = 11001;
+
+            NumberSystemFuntions.MainClass main = new NumberSystemFuntions.MainClass();
+            Int64 actual = main.DecimaltoBinary(num);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void DecimaltoOctalTest()
+        {
+            int num = 25;
+            int expected = 31;
+
+            NumberSystemFuntions.MainClass main = new NumberSystemFuntions.MainClass();
+            int actual = main.DecimaltoOctal(num);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void DecimaltoHexadecimalTest()
+        {
+            int num = 25;
+            string expected = "19";
+
+            NumberSystemFuntions.MainClass main = new NumberSystemFuntions.MainClass();
+            string actual = main.DecimalToHexadecimal(num);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
